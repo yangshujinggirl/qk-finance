3a015eb0c8a07a471dace55cd65d306b517e4b8d
lifengxia <lifengxia@bravowhale.com>
2020-08-24 20:47:38 +0800

    授权

--- finance/src/api/platformManage/RoleManage.js
+++ finance/src/api/platformManage/RoleManage.js
@@ -5,19 +5,23 @@ import Req from '../Req';
 export function getRoleList(values) {
     return Req.post(`/role/search`, values)
 }
+
 // 新增角色
 export function addRole(values) {
     return Req.post(`/role/addOrUpdate`, values)
 }
+
 // 删除角色
 export function deleteRole(values) {
     return Req.post(`/role/deleteRole/${values}`)
 }
+
 // 授权树
 export function getPermissionTree(id) {
     return Req.post(`/role/getPermissionTree/${id}`)
 }
+
 // 授予权限2
-export function saveRolePermissionRef(values) {
-    return Req.post(`/role/saveRolePermissionRef`, values)
+export function saveRolePermissionRef({allPermissionId, roleid}) {
+    return Req.post(`/role/saveRolePermissionRef?allPermissionId=${allPermissionId}&roleid=${roleid}`, values)
 }
--- finance/src/pages/platformManage/RoleManage/components/AuthModal/index.js
+++ finance/src/pages/platformManage/RoleManage/components/AuthModal/index.js
@@ -1,59 +1,52 @@
-import { Form, Tree, Row, Radio, Col, Modal, Checkbox, Button, Input } from 'antd';
+import {Tree, Modal} from 'antd';
 import './index.less';
-import { YtMessage } from 'common';
-import {saveRolePermissionRef,getPermissionTree} from '../../../../../api/platformManage/RoleManage.js'
-import { useState, useEffect } from 'react';
+import {YtMessage} from 'common';
+import {saveRolePermissionRef, getPermissionTree} from '../../../../../api/platformManage/RoleManage.js'
+import {useState, useEffect} from 'react';
 
-
-const formItemLayout = {
-  labelCol: {
-    span: 6
-  },
-  wrapperCol: {
-    span: 14
-  }
-};
-const CreatModal=({...props})=>{
-  const {treeData,defaultSelectedKeys,roleid}=props;
+const AuthModal = ({...props}) => {
+    const {treeData, defaultSelectedKeys, roleid} = props;
     const [checkeds, setCheckeds] = useState([]);
     console.log(defaultSelectedKeys);
-    // const defaultExpandedKeys=treeData[0]&&treeData[0].key
-    const handleOk = async() => {
-    try {
-      saveRolePermissionRef({
-          allPermissionId:checkeds.join(','),
-          roleid
-      }).then(res=>{
-          YtMessage.success('操作成功');
-          props.onOk && props.onOk(values);
-      })
-    } catch (errorInfo) {
+    console.log(treeData[0]);
+    const handleOk = async () => {
+        try {
+            saveRolePermissionRef({
+                allPermissionId: checkeds.join(','),
+                roleid
+            }).then(res => {
+                YtMessage.success('操作成功');
+                props.onOk && props.onOk(values);
+            })
+        } catch (errorInfo) {
 
-        console.log("Failed:", errorInfo);
-    }
-  };
-  const handleCancel = (e) => {
-    props.onCancel()
-  };
-  const onCheck = (e) => {
-      setCheckeds(e)
-      console.log(e);
-  };
+            console.log("Failed:", errorInfo);
+        }
+    };
+    const handleCancel = (e) => {
+        props.onCancel()
+    };
+    const onCheck = (e) => {
+        setCheckeds(e)
+        console.log(e);
+    };
 
-  return (
-      <Modal
-        width={520}
-        title="授予权限"
-        visible={props.visible===2}
-        onOk={handleOk}
-        onCancel={handleCancel}
-        className="creat-modal">
-          <Tree
-             checkable
-             defaultSelectedKeys={defaultSelectedKeys}
-              onCheck={onCheck}/>
-      </Modal>
-  );
+    return (
+        <Modal
+            width={520}
+            title="授予权限"
+            visible={props.visible === 2}
+            onOk={handleOk}
+            onCancel = {handleCancel}
+            className = "creat-modal">
+                <Tree
+                    checkable
+                    defaultSelectedKeys={defaultSelectedKeys}
+                    onCheck={onCheck}
+                    treeData={treeData}/>
+            </Modal>
+)
+    ;
 }
 
-export default CreatModal;
+export default AuthModal;
